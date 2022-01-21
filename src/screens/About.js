3f9b1f5f5880891d76bb38d8c0213aaf67c9f136
Lawrencesalinas <lawrencesalinas@yahoo.com>
2022-01-21 03:22:13 -0500

    about section

--- src/screens/About.js
+++ src/screens/About.js
@@ -4,8 +4,13 @@ import './About.css'
 const About = () => {
     return (
         <div className = 'about' id = 'about'>
-        <h1>I create products </h1>
-        <p>My name is Lawrence Salinas. I am a web designer and developer.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
+        <h1>About Me</h1>
+     <p>About Me
+I am a restaurant manager turned Software Engineer that enjoys the art of blending creativity and efficiency to address challenging problems. I am currently looking to work with a team of motivated individuals to create products that provide solutions to real-world problems. When I am not coding, I enjoy competitive strategy and tactics games at the highest level and am an aspiring amateur Brazilian Jiu-Jitsu tactician. I look forward to facing challenging problems that future roles will provide and completing the journey it takes to overcome them. Ice Breaker Overview:
+
+Favorite Shows: The Office, Parks and Recreation, Brooklyn Nine-Nine, Community, Modern Family, Adventure Time, Regular Show, Gravity Falls, Rick and Morty
+Favorite Games: Chess, One Night Ultimate Werewolf, Duelyst, Super Smash Bros Melee, Mario Party 4, Legends of Runeterra, Paladins, Nintendoland, Puyo Puyo Tetris
+Hobbies: Food, Travel, Learning, Brazilian Jiu-Jitsu, UFC/MMA, Working Out, Smart Home Tech, Wearables</p>
         </div>
     )
 }
--- src/screens/Skills.js
+++ src/screens/Skills.js
@@ -1,22 +1,32 @@
-import React from 'react';
-import './Skills.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
+import React from "react";
+import "./Skills.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Button } from "@mui/material";
 
 function Skills() {
   return (
-
-  <div className='skills' id = 'skills'>
-<div className='skills_logos'>
-<h1>HELLLLLLLLOOOO</h1>
-<i class="fab fa-js fa-5x"></i>
-<i class="fab fa-python fa-5x"></i>
-
-</div>
-</div>
-
-
- 
-  )
+    <div className="skills" id="skills">
+      <h1>Skills</h1>
+      <div className="skills_logos">
+          <i class="logo fab fa-js fa-5x"></i>
+          <i class="logo fab fa-python fa-5x"></i>
+          <i class="logo fab fa-html5 fa-5x"></i>
+          <i class="logo fab fa-node fa-5x"></i>
+          <i class="logo fab fa-react fa-5x"></i>
+        <i class="logo fab fa-css3-alt fa-5x"></i>
+      </div>
+      <div className="skills_logos2">
+      
+        <img className="logo2" src="img/postgres.svg" />
+        <img className="logo2" src="img/mongo.svg" />
+        <img className="logo2" src="img/dj.svg" />
+        <img className="logo2" src="img/express.svg" />
+      </div>
+      <form action ="#home">
+      <Button className='resume' variant="outlined" target="_blank" type="submit">Check out my resume</Button>
+      </form>
+    </div>
+  );
 }
 
 export default Skills;
